040d7189c572c733198b462000c515f8cf21bd17
Rondy Sousa <rondysousa@gmail.com>
2016-11-13 21:24:14 -0200

    Refactor with Validator (from ActiveModel support)

--- app/models/check_user_is_allowed_to_create_project.rb
+++ app/models/check_user_is_allowed_to_create_project.rb
@@ -1,39 +1,69 @@
 class CheckUserIsAllowedToCreateProject
   def call(user)
-    if user_is_not_manager?(user)
-      OpenStruct.new(
-        is_allowed?: false,
-        error_reason: :user_must_be_a_manager
-      )
-    elsif user_is_beyond_the_projects_count_limit_rule?(user)
-      OpenStruct.new(
-        is_allowed?: false,
-        error_reason: :user_must_be_within_the_projects_count_limit_rule
-      )
-    elsif project_creation_config_is_blocked?(user)
-      OpenStruct.new(
-        is_allowed?: false,
-        error_reason: :project_creation_config_must_be_no_blocked
-      )
+    validator = build_validator_for(user)
+
+    if validator.valid?
+      build_result_for_allowed_permission
     else
-      OpenStruct.new(
-        is_allowed?: true,
-        error_reason: nil
+      error_reason = extract_error_reason_from(validator)
+
+      build_result_for_denied_permission(
+        error_reason: error_reason
       )
     end
   end
 
   private
 
-  def user_is_not_manager?(user)
-    !user.manager?
+  def build_validator_for(user)
+    Validator.new(user: user)
   end
 
-  def user_is_beyond_the_projects_count_limit_rule?(user)
-    user.beyond_the_projects_count_limit_rule?
+  def extract_error_reason_from(validator)
+    validator.errors.to_a.first.to_sym
   end
 
-  def project_creation_config_is_blocked?(user)
-    user.project_creation_config_is_blocked?
+  def build_result_for_allowed_permission
+    OpenStruct.new(
+      is_allowed?: true,
+      error_reason: nil
+    )
+  end
+
+  def build_result_for_denied_permission(error_reason:)
+    OpenStruct.new(
+      is_allowed?: false,
+      error_reason: error_reason
+    )
+  end
+
+  class Validator
+    include ActiveModel::Model
+
+    attr_accessor :user
+
+    validate :user_must_be_manager
+    validate :user_must_be_within_the_projects_count_limit_rule
+    validate :project_creation_config_must_be_no_blocked
+
+    private
+
+    def user_must_be_manager
+      if !user.manager?
+        errors.add(:base, 'user_must_be_a_manager')
+      end
+    end
+
+    def user_must_be_within_the_projects_count_limit_rule
+      if user.beyond_the_projects_count_limit_rule?
+        errors.add(:base, 'user_must_be_within_the_projects_count_limit_rule')
+      end
+    end
+
+    def project_creation_config_must_be_no_blocked
+      if user.project_creation_config_is_blocked?
+        errors.add(:base, 'project_creation_config_must_be_no_blocked')
+      end
+    end
   end
 end
